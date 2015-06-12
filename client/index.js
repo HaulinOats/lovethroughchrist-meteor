128bfc25fb8adf8635daa0ab0554eff2e9202f25
brett84c <brett84c@gmail.com>
2015-06-12 17:07:55 -0400

    first commit

---
+++ client/index.js
@@ -0,0 +1,3 @@
+Session.setDefault('counter', 0);
+
+
---
+++ server/config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '289256867900965',
+    secret: '813b5631116afc377fe572435f7776ad'
+});
